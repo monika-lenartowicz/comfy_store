6f435dec1d4e000f5a268c2d230b26998aa08737
Monika Lenartowicz <nika.nel@interia.pl>
2023-01-05 16:05:04 +0100

    added setupStore function

--- src/store.js
+++ src/store.js
@@ -1,5 +1,17 @@
-import { getStorageItem, setStorageItem } from './utils.js';
+import { getStorageItem, setStorageItem } from "./utils.js";
 let store = [];
-const setupStore = () => {};
+const setupStore = products => {
+	store = products.map(product => {
+		const {
+			id,
+			fields: { featured, name, price, colors, company, image: img },
+		} = product;
+		const image = img[0].thumbnails.large.url;
+		return { featured, name, price, colors, company, image };
+	});
+};
+console.log(store);
+
 const findProduct = () => {};
+
 export { store, setupStore, findProduct };
