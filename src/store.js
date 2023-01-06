02d9ba70bd69f903a20e08eb55daf04d0ee81a9a
Monika Lenartowicz <nika.nel@interia.pl>
2023-01-06 10:55:58 +0100

    Set storage item and function get storage item

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { getStorageItem, setStorageItem } from "./utils.js";
-let store = [];
+let store = getStorageItem("store");
 const setupStore = products => {
 	store = products.map(product => {
 		const {
@@ -9,7 +9,9 @@ const setupStore = products => {
 		const image = img[0].thumbnails.large.url;
 		return { featured, name, price, colors, company, image };
 	});
+	setStorageItem("store", store);
 };
+
 console.log(store);
 
 const findProduct = () => {};
