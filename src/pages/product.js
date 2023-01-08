4367f02484e10c864c9e2862e2844b1ca220c3a6
Monika Lenartowicz <nika.nel@interia.pl>
2023-01-08 11:57:50 +0100

    added catch id-product function

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,24 +1,48 @@
 // global imports
-import '../toggleSidebar.js';
-import '../cart/toggleCart.js';
-import '../cart/setupCart.js';
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 // specific
-import { addToCart } from '../cart/setupCart.js';
-import { singleProductUrl, getElement, formatPrice } from '../utils.js';
+import { addToCart } from "../cart/setupCart.js";
+import { singleProductUrl, getElement, formatPrice } from "../utils.js";
 
 // selections
-// const loading = getElement('.page-loading');
-// const centerDOM = getElement('.single-product-center');
-// const pageTitleDOM = getElement('.page-hero-title');
-// const imgDOM = getElement('.single-product-img');
-// const titleDOM = getElement('.single-product-title');
-// const companyDOM = getElement('.single-product-company');
-// const priceDOM = getElement('.single-product-price');
-// const colorsDOM = getElement('.single-product-colors');
-// const descDOM = getElement('.single-product-desc');
-// const cartBtn = getElement('.addToCartBtn');
+const loading = getElement(".page-loading");
+const centerDOM = getElement(".single-product__center");
+const pageTitleDOM = getElement(".page-hero__title");
+const imgDOM = getElement(".single-product__img");
+const titleDOM = getElement(".single-product__title");
+const companyDOM = getElement(".single-product__company");
+const priceDOM = getElement(".single-product__price");
+const colorsDOM = getElement(".single-product__colors");
+const descDOM = getElement(".single-product__desc");
+const cartBtn = getElement(".addToCartBtn");
 
 // cart product
-// let productID;
+let productID;
 
 // show product when page loads
+window.addEventListener("DOMContentLoaded", async function () {
+	const urlID = window.location.search;
+	// const urlID = "?id=hello";
+	try {
+		const response = await fetch(`${singleProductUrl}${urlID}`);
+		if (response.status >= 200 && response.status <= 299) {
+			const product = await response.json();
+			console.log(product);
+		} else {
+			console.log(response.status, response.statusText);
+			centerDOM.innerHTML = `
+      <div>
+        <h3 class="error">... ${response.status} ${response.statusText},<br><br>
+         sorry, something went wrong</h3>
+        <a href="index.html" class="btn">back home</a>
+      </div>
+      `;
+		}
+	} catch (error) {
+		console.log(error);
+	}
+
+	loading.style.display = "none";
+});
