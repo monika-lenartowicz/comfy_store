65b2931dec93ba1368b9705c83a34c698fc75752
Monika Lenartowicz <nika.nel@interia.pl>
2023-01-08 15:02:31 +0100

    added single product  display function

--- src/pages/product.js
+++ src/pages/product.js
@@ -29,13 +29,33 @@ window.addEventListener("DOMContentLoaded", async function () {
 		const response = await fetch(`${singleProductUrl}${urlID}`);
 		if (response.status >= 200 && response.status <= 299) {
 			const product = await response.json();
-			console.log(product);
+			// grab data
+			const { id, fields } = product;
+			productID = id;
+			const { name, company, price, colors, description } = fields;
+			const image = fields.image[0].thumbnails.large.url;
+
+			// set values
+			document.title = `${name.toUpperCase()} | Comfy`;
+			pageTitleDOM.textContent = `Home / ${name}`;
+			imgDOM.src = image;
+			titleDOM.textContent = name;
+			companyDOM.textContent = `by ${company}`;
+			priceDOM.textContent = `${formatPrice(price)}`;
+			descDOM.textContent = `${description}`;
+			colors.forEach(color => {
+				const span = document.createElement("span");
+				span.classList.add("product-color");
+				span.style.backgroundColor = `${color}`;
+				colorsDOM.appendChild(span);
+			});
 		} else {
 			console.log(response.status, response.statusText);
 			centerDOM.innerHTML = `
       <div>
         <h3 class="error">... ${response.status} ${response.statusText},<br><br>
-         sorry, something went wrong</h3>
+         sorry, something went wrong
+        </h3>
         <a href="index.html" class="btn">back home</a>
       </div>
       `;
@@ -45,4 +65,8 @@ window.addEventListener("DOMContentLoaded", async function () {
 	}
 
 	loading.style.display = "none";
+});
+
+cartBtn.addEventListener("click", function () {
+	addToCart(productID);
 });
